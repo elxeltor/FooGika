e2ad6f213cde301dbc0d446b6252330de5ed659d
elxeltor <el.xeltor@gmail.com>
2012-03-05 22:17:52 -0500

    played a bit with dynamic buttons instantiation

--- src/xeltor/games/foogika/activities/FooGikaMenu.java
+++ src/xeltor/games/foogika/activities/FooGikaMenu.java
@@ -2,6 +2,8 @@ package xeltor.games.foogika.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class FooGikaMenu extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,16 @@ public class FooGikaMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.menulayout);
+        RelativeLayout.LayoutParams mylayout = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.FILL_PARENT,
+                RelativeLayout.LayoutParams.FILL_PARENT);
+        Button btn=new Button(this);
+        btn.setId(133);
+        btn.setBackgroundResource(R.drawable.trololo);
+        btn.setText("MyButton");
+        btn.setLayoutParams(mylayout);
+        layout.addView(btn);
     }
 }
