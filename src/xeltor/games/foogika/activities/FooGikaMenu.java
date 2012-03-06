e2d99e2af2b4b7b1f0a425282fd96e6774153eac
elxeltor <el.xeltor@gmail.com>
2012-03-06 00:04:19 -0500

    i added the GameSetting Activity, i probably should rename it to GamePrepare or something. i also started game_setting xml file but it's far from finished

--- src/xeltor/games/foogika/activities/FooGikaMenu.java
+++ src/xeltor/games/foogika/activities/FooGikaMenu.java
@@ -1,7 +1,10 @@
 package xeltor.games.foogika.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -11,16 +14,25 @@ public class FooGikaMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+		final Intent intent = new Intent(this, GameSettings.class);
         
         RelativeLayout layout = (RelativeLayout)findViewById(R.id.menulayout);
         RelativeLayout.LayoutParams mylayout = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.FILL_PARENT,
                 RelativeLayout.LayoutParams.FILL_PARENT);
         Button btn=new Button(this);
-        btn.setId(133);
         btn.setBackgroundResource(R.drawable.trololo);
         btn.setText("MyButton");
         btn.setLayoutParams(mylayout);
+        btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(intent);
+			}
+		});
         layout.addView(btn);
     }
 }
---
+++ src/xeltor/games/foogika/activities/GameSettings.java
@@ -0,0 +1,12 @@
+package xeltor.games.foogika.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameSettings extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_settings);
+	}
+}
